701e8c199b42114267b51492c6d6614e37553c75
Arthur Bergamo <arthur_bergamo@hotmail.com>
2025-05-08 16:05:27 -0300

    commitando mudanças

--- src/Mockup.jsx
+++ src/Mockup.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './Mockup.css';
 import logo from "./assets/image/logoAgrok.jpeg";
+import './index.css'
 
 export default function App() {
     const [currentApp, setCurrentApp] = useState('home');
